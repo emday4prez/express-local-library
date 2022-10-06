3a387c0e4f062649e027c5087372904a33e62b53
Emerson <emerson.day@gmail.com>
2022-10-06 14:16:08 -0700

    setup project

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/cool', (req, res, next) => {
+  res.send(`youre so cool`)
+})
+module.exports = router;
