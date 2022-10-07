4e7bf0436648e82e1989e09289514a52d6080a1e
Emerson <emerson.day@gmail.com>
2022-10-07 11:15:14 -0700

    added routes and dummy controllers

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+
 const mongoose = require("mongoose");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
-
 
 // Set up default mongoose connection
 const mongoDB = `${process.env.MONGODB_URI}`;
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
